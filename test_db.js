f63a0e621e4134515b7aea217ac96fff0c4138da
nxy-ldw <2947543703@qq.com>
2026-09-02 20:11:25 +0800

    fix: 移除管理员注册入口, 仅保留3个预设管理员账号

---
+++ test_db.js
@@ -0,0 +1,7 @@
+﻿const db = require('./config/database');
+console.log('Tables:', Object.keys(db._raw).filter(k => !k.startsWith('_')).join(', '));
+console.log('Has settings:', 'settings' in db._raw);
+console.log('Has system_config:', 'system_config' in db._raw);
+const sc = db._raw.system_config || [];
+console.log('system_config entries:', sc.length);
+if (sc.length > 0) console.log('First entry:', JSON.stringify(sc[0]));
